266460f2eaeca043962b0714a7c4c2e11288c5f8
vivianbarclay <vivian.m.barclay@gmail.com>
2025-01-28 18:56:40 +0000

    finished project

--- app.rb
+++ app.rb
@@ -83,9 +83,9 @@ get("/") do
   <div><a href='/rock'>Play Rock</a></div>
   <div><a href='/paper'>Play Paper</a></div>
   <div><a href='/scissors'>Play Scissors</a></div>
-  
+
   <h1>Welcome to Rock-Paper-Scissors!</h1>
-  <p>From Wikipedia: <a href='https://en.wikipedia.org/wiki/Rock_paper_scissors' target='_blank'>Wikipedia</a></p>
+  <p>From <a href='https://en.wikipedia.org/wiki/Rock_paper_scissors' target='_blank'>Wikipedia</a>:</p>
 
   <p> Rock-paper-scissors (also known as paper, scissors, stone or other variants) is a hand game usually played between two people, in which each player simultaneously forms one of three shapes with an outstretched hand. </p>
 
